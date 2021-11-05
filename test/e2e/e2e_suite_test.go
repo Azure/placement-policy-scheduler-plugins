69ca3590cb4fe0dbe9be5d2e5e023af7d8dff6b7
Heba Elayoty <hebaelayoty@gmail.com>
2021-11-05 13:23:06 -0700

    add node to the framework

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -15,7 +15,9 @@ func TestE2e(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-
+	// Ensure cluster is running and ready before starting tests
+	By("Checking KinD Cluster is up and ready")
+	By("Configuring Placement Policy plugins")
 })
 
 var _ = AfterSuite(func() {
---
+++ test/e2e/framework/cleanup.go
@@ -0,0 +1,56 @@
+//go:build e2e
+// +build e2e
+
+// Vendored from Azure/secrets-store-csi-driver-provider-azure
+//  * tag: v1.0.0-rc.0,
+//  * link: https://github.com/Azure/secrets-store-csi-driver-provider-azure/blob/b458c5cd05/test/e2e/framework/cleanup.go
+
+package framework
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/Azure/placement-policy-scheduler-plugins/test/e2e/framework"
+	"github.com/Azure/placement-policy-scheduler-plugins/test/e2e/framework/namespace"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// CleanupInput is the input for Cleanup.
+type CleanupInput struct {
+	Namespace *corev1.Namespace
+	Getter    framework.Getter
+	Lister    framework.Lister
+	Deleter   framework.Deleter
+}
+
+// Cleanup cleans up the test environment after a test case is finished running.
+func Cleanup(input CleanupInput) {
+	Expect(input.Namespace).NotTo(BeNil(), "input.Namespace is required for e2e.Cleanup")
+	Expect(input.Getter).NotTo(BeNil(), "input.Getter is required for e2e.Cleanup")
+	Expect(input.Lister).NotTo(BeNil(), "input.Lister is required for e2e.Cleanup")
+	Expect(input.Deleter).NotTo(BeNil(), "input.Deleter is required for e2e.Cleanup")
+
+	By(fmt.Sprintf("Deleting all pods in %s", input.Namespace.Name))
+
+	podList := &corev1.PodList{}
+	Expect(input.Lister.List(context.TODO(), podList, client.InNamespace(input.Namespace.Name))).Should(Succeed())
+	for _, pod := range podList.Items {
+		Expect(input.Deleter.Delete(context.TODO(), &pod)).Should(Succeed())
+	}
+
+	Eventually(func() bool {
+		podList := &corev1.PodList{}
+		Expect(input.Lister.List(context.TODO(), podList, client.InNamespace(input.Namespace.Name))).Should(Succeed())
+		return len(podList.Items) == 0
+	})
+
+	namespace.Delete(namespace.DeleteInput{
+		Deleter:   input.Deleter,
+		Getter:    input.Getter,
+		Namespace: input.Namespace,
+	})
+}
